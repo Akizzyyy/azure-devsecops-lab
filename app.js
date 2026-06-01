b69aaaab2ac43249fb406068e159fad1a1ff816b
Akizzyyy <tayo6135@gmail.com>
2026-06-01 23:07:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
 
+const demoSecret = "DEMO_SECRET = fakepassword123";
+
+app.get("/", (req, res) => {
+  const name = req.query.name || "World";
+  res.send("Hello " + name);
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
